6c57935ae6e52b922bc60979d82c38a4526a7710
wonjae <choi.wonjae@jinnhyun.com>
2023-09-15 17:49:47 +0900

    경로수정 test

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,14 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                $urlArr = explode('/', $request->url());
+                $res = end($urlArr);
+                
+                if($res == "user") {
+                    return redirect('/user/home');
+                } else {
+                    return redirect('/home');
+                }
             }
         }
 
