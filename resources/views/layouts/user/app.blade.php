9645eb9acb143bf03ff2ee0b247cf304b449776a
wonjae <choi.wonjae@jinnhyun.com>
2023-09-18 13:41:32 +0900

    asdasdas

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -42,7 +42,6 @@
                 <main class="main-content border-radius-lg">
                     @yield('content')
                 </main>
-            @include('components.fixed-plugin')
         @endif
     @endauth
 
@@ -60,8 +59,6 @@
             Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
         }
     </script>
-    <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="/assets/js/argon-dashboard.js"></script>
     @stack('js');
