5209dfede6e57fd56185358a7f6756ec48a7cce0
wonjae <choi.wonjae@jinnhyun.com>
2023-09-20 14:14:22 +0900

    jquery add

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -48,6 +48,7 @@
     <!--   Core JS Files   -->
     <script src="/assets/js/core/popper.min.js"></script>
     <script src="/assets/js/core/bootstrap.min.js"></script>
+    <script src="/assets/js/core/jquery-3.7.1.min.js"></script>
     <script src="/assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="/assets/js/plugins/smooth-scrollbar.min.js"></script>
     <script src="/assets/js/plugins/jq-signature.min.js"></script>
