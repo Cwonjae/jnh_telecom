5500534595b0f27f8f6d97158678a20eed44b06b
wonjae <choi.wonjae@jinnhyun.com>
2023-09-21 17:09:16 +0900

    lkjlkjlkj

--- resources/views/pages/user/tables_register.blade.php
+++ resources/views/pages/user/tables_register.blade.php
@@ -65,58 +65,73 @@
                 /**
                  * 유효성검사를 스크립트로 해보자
                  */
-                if(!applicant) {
-                    key = "applicant";
-                    value = "The name field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!nationality) {
-                    key = "nationality";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!passport) {
-                    key = "passport";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!dateofbirth) {
-                    key = "dateofbirth";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!gander) {
-                    key = "gander";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!device) {
-                    key = "device";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!devicemodel) {
-                    key = "devicemodel";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!osversion) {
-                    key = "osversion";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!imeinumber) {
-                    key = "imeinumber";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!plan) {
-                    key = "plan";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!callservice) {
-                    key = "callservice";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!service) {
-                    key = "service";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
-                } else if(!connectivity) {
-                    key = "connectivity";
-                    value = "The "+key+" field is required.";
-                    $('p#error_'+key).text(value);
+                if(!applicant || !nationality || !passport || !dateofbirth || !gander || !gander || !device || !devicemodel || !osversion || !imeinumber || !plan || !callservice || !service || !connectivity) {
+                    if(!applicant) {
+                        key = "applicant";
+                        value = "The name field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!nationality) {
+                        key = "nationality";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!passport) {
+                        key = "passport";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!dateofbirth) {
+                        key = "dateofbirth";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!gander) {
+                        key = "gander";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!device) {
+                        key = "device";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!devicemodel) {
+                        key = "devicemodel";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!osversion) {
+                        key = "osversion";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!imeinumber) {
+                        key = "imeinumber";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!plan) {
+                        key = "plan";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!callservice) {
+                        key = "callservice";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!service) {
+                        key = "service";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    if(!connectivity) {
+                        key = "connectivity";
+                        value = "The "+key+" field is required.";
+                        $('p#error_'+key).text(value);
+                    }
+                    return false;
                 }
 
                 var url = $(this).attr('data-action');
