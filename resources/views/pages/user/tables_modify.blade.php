c43ffe3dcf31c2f6151e9db79bcba4db51f6f1cd
wonjae <choi.wonjae@jinnhyun.com>
2023-09-25 15:23:22 +0900

    qwewqewqewq

--- resources/views/pages/user/tables_modify.blade.php
+++ resources/views/pages/user/tables_modify.blade.php
@@ -113,53 +113,53 @@
                             @csrf
                                 <div class="flex flex-col mb-3">
                                     <h6>Name <span style="color:red">*</span></h6>
-                                    <input type="text" name="applicant" class="form-control" placeholder="Your Full Name" aria-label="Name" value="{{ $cell_phones->cpb_applicant }}" id="applicant">
+                                    <input type="text" name="applicant" class="form-control" placeholder="Your Full Name" aria-label="Name" value="{{ $cell_phones[0]->cpb_applicant }}" id="applicant">
                                     @error('applicant') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>Nationality <span style="color:red">*</span></h6>
-                                    <input type="text" name="nationality" class="form-control" placeholder="Nationality" aria-label="Nationality" value="{{ $cell_phones->cpb_nationality }}" autocomplete="off" id="inputSearch">
+                                    <input type="text" name="nationality" class="form-control" placeholder="Nationality" aria-label="Nationality" value="{{ $cell_phones[0]->cpb_nationality }}" autocomplete="off" id="inputSearch">
                                     @error('nationality') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>Passport <span style="color:red">*</span></h6>
                                     <input type="file" name="passport" class="form-control" aria-label="Passport" id="passport">
                                     @error('passport') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
-                                    @if($cell_phones->ppu_filename)
+                                    @if($cell_phones[0]->ppu_filename)
                                         <a href="/images/passport/g7ikW7BwO8raRXjcytI9UdEdAPSOJti5caikJxEY.png" target="_blank">저장된 여권 보기</a>
                                     @endif
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>Date Of Birth <span style="color:red">*</span></h6>
-                                    <input type="text" name="dateofbirth" class="form-control" placeholder="MM-DD-YYYY" aria-label="Date Of Birth" value="{{ $cell_phones->cpb_dateofbirth }}" id="dateofbirth" maxlength="11">
+                                    <input type="text" name="dateofbirth" class="form-control" placeholder="MM-DD-YYYY" aria-label="Date Of Birth" value="{{ $cell_phones[0]->cpb_dateofbirth }}" id="dateofbirth" maxlength="11">
                                     @error('dateofbirth') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="form-radio form-check-info text-start">
                                     <h6>Gender <span style="color:red">*</span></h6>
-                                    <input class="form-radio-input" type="radio" name="gender" id="flexRadioDefault_m" value="male" @if($cell_phones->cpb_gender == "male") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="gender" id="flexRadioDefault_m" value="male" @if($cell_phones[0]->cpb_gender == "male") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_m">Male</label>
-                                    <input class="form-radio-input" type="radio" name="gender" id="flexRadioDefault_f" value="female" @if($cell_phones->cpb_gender == "female") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="gender" id="flexRadioDefault_f" value="female" @if($cell_phones[0]->cpb_gender == "female") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_f">FeMale</label>
                                     @error('gender') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="form-radio form-check-info text-start">
                                     <h6>Device <span style="color:red">*</span></h6>
-                                    <input class="form-radio-input" type="radio" name="device" id="flexRadioDefault_ap" value="apple" @if($cell_phones->cpb_device == "apple") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="device" id="flexRadioDefault_ap" value="apple" @if($cell_phones[0]->cpb_device == "apple") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_ap">Apple</label>
-                                    <input class="form-radio-input" type="radio" name="device" id="flexRadioDefault_s" value="samsung" @if($cell_phones->cpb_device == "samsung") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="device" id="flexRadioDefault_s" value="samsung" @if($cell_phones[0]->cpb_device == "samsung") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_s">Samsung</label>
-                                    <input class="form-radio-input" type="radio" name="device" id="flexRadioDefault_o" value="other" @if($cell_phones->cpb_device == "other") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="device" id="flexRadioDefault_o" value="other" @if($cell_phones[0]->cpb_device == "other") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_o">Other</label>
                                     @error('device') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>Device Model <span style="color:red">*</span></h6>
-                                    <input type="text" name="devicemodel" class="form-control" placeholder="Ex) Iphone 13, Iphone 13 mini, Galaxy S22, ETC" aria-label="Device Model" value="{{ $cell_phones->cpb_devicemodel }}" id="devicemodel">
+                                    <input type="text" name="devicemodel" class="form-control" placeholder="Ex) Iphone 13, Iphone 13 mini, Galaxy S22, ETC" aria-label="Device Model" value="{{ $cell_phones[0]->cpb_devicemodel }}" id="devicemodel">
                                     @error('devicemodel') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>OS Version <span style="color:red">*</span></h6>
-                                    <input type="text" name="osversion" class="form-control" placeholder="Ex) Android Version 13, IOS version 16.5" aria-label="Os Version" value="{{ $cell_phones->cpb_osversion }}" id="osversion">
+                                    <input type="text" name="osversion" class="form-control" placeholder="Ex) Android Version 13, IOS version 16.5" aria-label="Os Version" value="{{ $cell_phones[0]->cpb_osversion }}" id="osversion">
                                     @error('osversion') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
@@ -168,7 +168,7 @@
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>IMEI Number <span style="color:red">*</span></h6>
-                                    <input type="text" name="imeinumber" class="form-control" placeholder="IMEI Number" aria-label="IMEI Number" value="{{ $cell_phones->cpb_imeinumber }}" id="imeinumber">
+                                    <input type="text" name="imeinumber" class="form-control" placeholder="IMEI Number" aria-label="IMEI Number" value="{{ $cell_phones[0]->cpb_imeinumber }}" id="imeinumber">
                                     @error('imeinumber') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
@@ -188,14 +188,14 @@
                                 </div>
                                 <div class="form-radio form-check-info text-start">
                                     <h6>Plan <span style="color:red">*</span></h6>
-                                    <input class="form-radio-input" type="radio" name="plan" id="flexRadioDefault_p" value="ok" @if($cell_phones->cpb_plan == "ok") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="plan" id="flexRadioDefault_p" value="ok" @if($cell_phones[0]->cpb_plan == "ok") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_p">30,000 KRW($23)</label>
                                     <p>Please note that devices purchased from the United States may not be compatible with our 5G plan.</p>
                                     @error('plan') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>Please choose the last four digits of your phone number</h6>
-                                    <input type="text" name="chooselastnumber" class="form-control" placeholder="Please choose the last four digits of your phone number." aria-label="Please choose the last four digits of your phone number." value="{{ $cell_phones->cpb_chooselastnumber }}" onKeyPress="javascript:checkInputNum();" maxlength="4" id="chooselastnumber">
+                                    <input type="text" name="chooselastnumber" class="form-control" placeholder="Please choose the last four digits of your phone number." aria-label="Please choose the last four digits of your phone number." value="{{ $cell_phones[0]->cpb_chooselastnumber }}" onKeyPress="javascript:checkInputNum();" maxlength="4" id="chooselastnumber">
                                     @error('chooselastnumber') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                     <p>If the phone number you have chosen is already taken, please note that it can be activated with different last four digits.</p>
                                 </div>
@@ -208,21 +208,21 @@
                                     @error('signaturetxt') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                     <input type="hidden" id="signature_txt" name="signaturetxt" value="{{ old('signaturetxt') }}"/>
                                     <div id="signature">
-                                    @if($cell_phones->stu_filename)
-                                        <img id="signature_img" src="{{ $cell_phones->stu_base64 }}"/>
+                                    @if($cell_phones[0]->stu_filename)
+                                        <img id="signature_img" src="{{ $cell_phones[0]->stu_base64 }}"/>
                                     @endif
                                     </div>
                                 </div>
                                 <div class="flex flex-col mb-3">
                                     <h6>Referral</h6>
-                                    <input type="text" name="referral" class="form-control" placeholder="Please enter the referral's email address, phone number and name" aria-label="Referral" value="{{ $cell_phones->cpb_referral }}" id="referral">
+                                    <input type="text" name="referral" class="form-control" placeholder="Please enter the referral's email address, phone number and name" aria-label="Referral" value="{{ $cell_phones[0]->cpb_referral }}" id="referral">
                                     @error('referral') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="form-radio form-check-info text-start">
                                     <h6>Add International Calling Service <span style="color:red">*</span></h6>
-                                    <input class="form-radio-input" type="radio" name="callservice" id="flexRadioDefault_y" value="yes" @if($cell_phones->cpb_callservice == "yes") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="callservice" id="flexRadioDefault_y" value="yes" @if($cell_phones[0]->cpb_callservice == "yes") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_y">Yes</label>
-                                    <input class="form-radio-input" type="radio" name="callservice" id="flexRadioDefault_n" value="no" @if($cell_phones->cpb_callservice == "no") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="callservice" id="flexRadioDefault_n" value="no" @if($cell_phones[0]->cpb_callservice == "no") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_n">No</label>
                                     @error('callservice') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                     <p>Extra $5/ month per line</p>
@@ -233,17 +233,17 @@
                                 </div>
                                 <div class="form-radio form-check-info text-start">
                                     <h6>Service <span style="color:red">*</span></h6>
-                                    <input class="form-radio-input" type="radio" name="service" id="flexRadioDefault_ag" value="annual_agreement" @if($cell_phones->cpb_service == "annual_agreement") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="service" id="flexRadioDefault_ag" value="annual_agreement" @if($cell_phones[0]->cpb_service == "annual_agreement") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_ag">Annual Agreement (25% discount)</label>
-                                    <input class="form-radio-input" type="radio" name="service" id="flexRadioDefault_mp" value="monthly_plan" @if($cell_phones->cpb_service == "monthly_plan") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="service" id="flexRadioDefault_mp" value="monthly_plan" @if($cell_phones[0]->cpb_service == "monthly_plan") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_mp">Monthly Plan</label>
                                     @error('service') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="form-radio form-check-info text-start">
                                     <h6>Connectivity <span style="color:red">*</span></h6>
-                                    <input class="form-radio-input" type="radio" name="connectivity" id="flexRadioDefault_4g" value="4g" @if($cell_phones->cpb_connectivity == "4g") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="connectivity" id="flexRadioDefault_4g" value="4g" @if($cell_phones[0]->cpb_connectivity == "4g") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_4g">4G</label>
-                                    <input class="form-radio-input" type="radio" name="connectivity" id="flexRadioDefault_5g" value="5g" @if($cell_phones->cpb_connectivity == "5g") checked @else @endif>
+                                    <input class="form-radio-input" type="radio" name="connectivity" id="flexRadioDefault_5g" value="5g" @if($cell_phones[0]->cpb_connectivity == "5g") checked @else @endif>
                                     <label class="form-radio-label" for="flexRadioDefault_5g">5G</label>
                                     @error('connectivity') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                 </div>
