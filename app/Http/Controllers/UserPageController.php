937bdf59964cdfc28c4ee2598ea86e5fd1643186
wonjae <choi.wonjae@jinnhyun.com>
2023-09-25 17:44:08 +0900

    modify update add

--- app/Http/Controllers/UserPageController.php
+++ app/Http/Controllers/UserPageController.php
@@ -189,6 +189,155 @@ class UserPageController extends Controller
         }
     }
 
+    public function modify_update(Request $request, string $page, $num) {
+        
+        $user_id_check = DB::table('users')->where('id', Auth::id())->value('id');
+        $user_name_check = DB::table('users')->where('id', Auth::id())->value('username');
+        $currentDateTime = Carbon::now()->timezone('Asia/Seoul');
+        $now_date_time = $currentDateTime->toDateTimeString();
+
+       if(DB::table('cellphone_boards')->where('u_id', Auth::id())->where('id', $num)->exists()) {
+
+            // Form validate 구성
+            $validated = $request->validate([
+                'applicant' => 'required',
+                'nationality' => 'required',
+                'dateofbirth' => 'required',
+                'gender' => 'required|in:male,female',
+                'device' => 'required|in:apple,samsung,other',
+                'devicemodel' => 'required',
+                'osversion' => 'required',
+                'imeinumber' => 'required',
+                'plan' => 'required|in:ok',
+                'signaturetxt' => 'required',
+                'callservice' => 'required|in:yes,no',
+                'service' => 'required|in:annual_agreement,monthly_plan',
+                'connectivity' => 'required|in:4g,5g',
+            ]);
+
+            //passport 수정할 경우
+            if($request->file('passport')) {
+                // PassPort Upload 구성
+                // $upload_file = $request->file('passport')->store('public/images/passport');
+                $upload_file = Storage::putFile('/public/images/passport/',$request->file('passport'));
+                if($upload_file) {
+                    $file_name = $request->file('passport')->getClientOriginalName();
+                    $random_explode = explode('/public/images/passport/', $upload_file);
+                    $extension_cut = explode('.png', $random_explode[1]);
+                    $random_file_name = $extension_cut[0];
+
+                    $passport_insert_id = DB::table('passport_uploads')->insertGetId([
+                        'u_id' => $user_id_check,
+                        'ppu_filename' => $file_name,
+                        'ppu_encode_filename' => $random_file_name,
+                        'create_at' => $now_date_time
+                    ]);
+
+                    $cellphone_update = DB::table('cellphone_boards')
+                                        ->where('u_id', Auth::id())
+                                        ->where('id', $num)
+                                        ->update([
+                                            'ppu_id' => $passport_insert_id,
+                                            'update_at' => $now_date_time
+                                        ]);
+                    if(!$cellphone_update) {
+                        return back()->with('error', 'The passport was not DB Insert successfully.');
+                    }
+                } else {
+                    return back()->with('error', 'The passport was not uploaded successfully.');
+                }
+            }
+
+            // Signature Upload 구성
+            $base64_img = $request->post('signaturetxt');
+            $base64_img = str_replace('data:image/png;base64,', '', $base64_img);
+            $base64_img = str_replace(' ', '+', $base64_img);
+            $base64_decoding_img = base64_decode($base64_img);
+
+            // Signature 수정할 경우
+            if(DB::table('signature_uploads')->where('stu_base64', $base64_img)->doesntExist()) {
+                $file_name = $user_name_check.time().'.png';
+                $signatures = Storage::put('/public/images/signatures/'.$file_name, $base64_decoding_img);
+                if($signatures) {
+                    $signature_insert_id = DB::table('signature_uploads')->insertGetId([
+                        'u_id' => $user_id_check,
+                        'stu_filename' => $file_name,
+                        'stu_base64' => $base64_img,
+                        'create_at' => $now_date_time
+                    ]);
+
+                    $cellphone_update = DB::table('cellphone_boards')
+                                        ->where('u_id', Auth::id())
+                                        ->where('id', $num)
+                                        ->update([
+                                            'stu_id' => $signature_insert_id,
+                                            'update_at' => $now_date_time
+                                        ]);
+                    if(!$cellphone_update) {
+                        return back()->with('error', 'The signature was not DB Insert successfully.');
+                    }
+                } else {
+                    return back()->with('error', 'The signature was not uploaded successfully.');
+                }
+            }
+
+            $applicant = $request->post('applicant');
+            $nationality = $request->post('nationality');
+            $dateofbirth = $request->post('dateofbirth');
+            $gender = $request->post('gender');
+            $device = $request->post('device');
+            $devicemodel = $request->post('devicemodel');
+            $osversion = $request->post('osversion');
+            $imeinumber = $request->post('imeinumber');
+            $plan = $request->post('plan');
+            $callservice = $request->post('callservice');
+            $service = $request->post('service');
+            $connectivity = $request->post('connectivity');
+
+            if($request->post('referral')) {
+                $referral = $request->post('referral');
+            } else {
+                $referral = null;
+            }
+
+            if($request->post('chooselastnumber')) {
+                $chooselastnumber = $request->post('chooselastnumber');
+            } else {
+                $chooselastnumber = null;
+            }
+
+
+            $cellphone_update = DB::table('cellphone_boards')
+                                ->where('u_id', Auth::id())
+                                ->where('id', $num)
+                                ->update([
+                                    'cpb_applicant' => $applicant,
+                                    'cpb_nationality' => $nationality,
+                                    'cpb_dateofbirth' => $dateofbirth,
+                                    'cpb_gender' => $gender,
+                                    'cpb_device' => $device,
+                                    'cpb_devicemodel' => $devicemodel,
+                                    'cpb_osversion' => $osversion,
+                                    'cpb_imeinumber' => $imeinumber,
+                                    'cpb_plan' => $plan,
+                                    'cpb_chooselastnumber' => $chooselastnumber,
+                                    'cpb_referral' => $referral,
+                                    'cpb_callservice' => $callservice,
+                                    'cpb_service' => $service,
+                                    'cpb_connectivity' => $connectivity,
+                                    'cpb_telecoms' => 'kt',
+                                    'update_at' => $now_date_time
+                                ]);
+
+            if($cellphone_insert_id) {
+                return redirect('/user/tables');
+            } else {
+                return back()->with('error', 'Cell phone opening modify failed.');
+            }
+       }
+        
+    }
+
 
     public function vr()
     {
