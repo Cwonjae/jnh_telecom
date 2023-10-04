87e5be2d3cedb1b44792523ce7b7548b18844f06
wonjae <choi.wonjae@jinnhyun.com>
2023-10-04 15:41:54 +0900

    DB use add

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
