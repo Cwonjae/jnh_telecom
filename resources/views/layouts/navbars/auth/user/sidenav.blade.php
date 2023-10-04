bec05cb79addf4da4490179de4751d6b52be6f4c
wonjae <choi.wonjae@jinnhyun.com>
2023-10-04 15:08:53 +0900

    qweasd

--- resources/views/layouts/navbars/auth/user/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/user/sidenav.blade.php
@@ -3,11 +3,8 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('userhome') }}"
-            target="_blank">
-            <img src="/img/logo-jnh-png.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold">User Dashboard</span>
-        </a>
+        <img src="/img/logo-jnh-png.png" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold">User Page</span>
     </div>
     <hr class="horizontal dark mt-0">
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
