b011ad7c4b8bdbd288a8e27ecb04b63bafb80b21
wonjae <choi.wonjae@jinnhyun.com>
2023-10-04 17:46:54 +0900

    qweasd

--- resources/views/pages/print.blade.php
+++ resources/views/pages/print.blade.php
@@ -1,20 +1,23 @@
 <style>
 .main_box1 {
-    background : "/img/tables/olleh_mobile_application_form_back_1.jpg"
+    
 }
 .main_box2 {
-    background : "/img/tables/olleh_mobile_application_form_back_2.jpg"
+
 }
 .main_box3 {
-    background : "/img/tables/olleh_mobile_application_form_back_3.jpg"
+    
 }
 </style>
 <div class="main_box1">
+    <img src="/img/tables/olleh_mobile_application_form_back_1.jpg">
 
 </div>
 <div class="main_box2">
+    <img src="/img/tables/olleh_mobile_application_form_back_2.jpg">
 
 </div>
 <div class="main_box3">
+    <img src="/img/tables/olleh_mobile_application_form_back_3.jpg">
 
 </div>
