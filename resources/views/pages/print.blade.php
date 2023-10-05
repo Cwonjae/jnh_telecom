1ac2b92f6307c342da4d371a4188999ed7084aed
wonjae <choi.wonjae@jinnhyun.com>
2023-10-05 09:40:31 +0900

    print page modify

--- resources/views/pages/print.blade.php
+++ resources/views/pages/print.blade.php
@@ -1,41 +1,39 @@
 <style>
+.print_main_box {
+    margin: 0 auto;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+    max-height: 100%;
+    background: gray;
+}
 .main_box1 {
-    position:relative;
+    width:inherit;
 }
-.main_box1 img {
-    position:absolute;
-    width:100%;
-    margin:0 auto;
-}
-.main_box2 {
-
-}
-.main_box2 img {
-    position:absolute;
-    width:100%;
-    margin:0 auto;
-}
-.main_box3 {
-    
-}
-.main_box3 img {
-    width:100%;
-    margin:0 auto;
+.img_box_1 {
+    background:url("/img/tables/olleh_mobile_application_form_back_1.jpg") center no-repeat;
 }
 </style>
-<div class="main_box1">
-    <img src="/img/tables/olleh_mobile_application_form_back_1.jpg">
-    <span class="applicant">{{ $cell_phones[0]->cpb_applicant }}</span>
-    <span class="applicant">{{ $cell_phones[0]->cpb_passportnumber }}</span>
-    <span class="applicant">{{ $cell_phones[0]->cpb_gender }}</span>
-    <span class="applicant">{{ $cell_phones[0]->email }}</span>
-    <span class="applicant">{{ $cell_phones[0]->cpb_chooselastnumber }}</span>
+
+<div class="print_main_box">
+    <div class="main_box1">
+        <div class="img_box_1">
+
+        </div>
+        <!-- <span class="applicant">{{ $cell_phones[0]->cpb_applicant }}</span>
+        <span class="applicant">{{ $cell_phones[0]->cpb_passportnumber }}</span>
+        <span class="applicant">{{ $cell_phones[0]->cpb_gender }}</span>
+        <span class="applicant">{{ $cell_phones[0]->email }}</span>
+        <span class="applicant">{{ $cell_phones[0]->cpb_chooselastnumber }}</span>
+    </div> -->
 </div>
-<div class="main_box2">
+<!-- <div class="main_box2">
     <img src="/img/tables/olleh_mobile_application_form_back_2.jpg">
 
 </div>
 <div class="main_box3">
     <img src="/img/tables/olleh_mobile_application_form_back_3.jpg">
 
-</div>
+</div> -->
