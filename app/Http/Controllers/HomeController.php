a6e6199a01c50acb18f07c1312de4af29d68f258
wonjae <choi.wonjae@jinnhyun.com>
2023-10-10 07:43:53 +0900

    qweqwe

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -64,24 +64,40 @@ class HomeController extends Controller
                         ->where('cpb_status', '<>', 'closing')
                         ->count();
 
-        $yester_user_check = ((($user_cnt ? $user_cnt : 0) - ($user_y_cnt ? $user_y_cnt : 0)) / ($user_y_cnt ? $user_y_cnt : 0)) * 100;
-        if($user_cnt >= $user_y_cnt) {
-            $yester_user_check = (($user_cnt ? $user_cnt : 0) / ($user_y_cnt ? $user_y_cnt : 0)) * 100;
+        if($cellphone_cnt > 0 || $cellphone_y_cnt > 0) {
+            $yester_user_check = (($user_cnt - $user_y_cnt) / $user_y_cnt) * 100;
+            if($user_cnt >= $user_y_cnt) {
+                $yester_user_check = ($user_cnt / $user_y_cnt) * 100;
+            }
+        } else {
+            $yester_user_check = 0;
         }
 
-        $yester_cellphone_check = ((($cellphone_cnt ? $cellphone_cnt : 0) - ($cellphone_y_cnt ? $cellphone_y_cnt : 0)) / ($cellphone_y_cnt ? $cellphone_y_cnt : 0)) * 100;
-        if($cellphone_cnt >= $cellphone_y_cnt) {
-            $yester_cellphone_check = (($cellphone_cnt ? $cellphone_cnt : 0) / ($cellphone_y_cnt ? $cellphone_y_cnt : 0)) * 100;
+        if($cellphone_cnt > 0 || $cellphone_y_cnt > 0) {
+            $yester_cellphone_check = (($cellphone_cnt - $cellphone_y_cnt) / $cellphone_y_cnt) * 100;
+            if($cellphone_cnt >= $cellphone_y_cnt) {
+                $yester_cellphone_check = ($usecellphone_cntr_cnt / $cellphone_y_cnt) * 100;
+            }
+        } else {
+            $yester_cellphone_check = 0;
         }
 
-        $yester_cellphone_done_check = ((($cellphone_done_cnt ? $cellphone_done_cnt : 0) - ($cellphone_y_done_cnt ? $cellphone_y_done_cnt : 0)) / ($cellphone_y_done_cnt ? $cellphone_y_done_cnt : 0)) * 100;
-        if($cellphone_done_cnt >= $cellphone_y_done_cnt) {
-            $yester_cellphone_done_check = (($cellphone_done_cnt ? $cellphone_done_cnt : 0) / ($cellphone_y_done_cnt ? $cellphone_y_done_cnt : 0)) * 100;
+        if($cellphone_done_cnt > 0 || $cellphone_y_done_cnt > 0) {
+            $yester_cellphone_done_check = (($cellphone_done_cnt - $cellphone_y_done_cnt) / $cellphone_y_done_cnt) * 100;
+            if($cellphone_done_cnt >= $cellphone_y_done_cnt) {
+                $yester_cellphone_done_check = ($cellphone_done_cnt / $cellphone_y_done_cnt) * 100;
+            }
+        } else {
+            $yester_cellphone_done_check = 0;
         }
 
-        $yester_cellphone_not_check = ((($cellphone_not_cnt ? $cellphone_not_cnt : 0) - ($cellphone_y_not_cnt ? $cellphone_y_not_cnt : 0)) / ($cellphone_y_not_cnt ? $cellphone_y_not_cnt : 0)) * 100;
-        if($cellphone_not_cnt >= $cellphone_y_not_cnt) {
-            $yester_cellphone_not_check = (($cellphone_not_cnt ? $cellphone_not_cnt : 0) / ($cellphone_y_not_cnt ? $cellphone_y_not_cnt : 0)) * 100;
+        if($cellphone_not_cnt > 0 || $cellphone_y_not_cnt > 0) {
+            $yester_cellphone_not_check = (($cellphone_not_cnt - $cellphone_y_not_cnt) / $cellphone_y_not_cnt) * 100;
+            if($cellphone_not_cnt >= $cellphone_y_not_cnt) {
+                $yester_cellphone_not_check = ($cellphone_not_cnt / $cellphone_y_not_cnt) * 100;
+            }
+        } else {
+            $yester_cellphone_not_check = 0;
         }
 
         return view('pages.dashboard', compact('user_cnt', 'user_y_cnt', 'cellphone_cnt', 'cellphone_y_cnt', 'cellphone_done_cnt', 'cellphone_y_done_cnt', 'cellphone_not_cnt', 'cellphone_y_not_cnt', 'yester_user_check', 'yester_cellphone_check', 'yester_cellphone_done_check', 'yester_cellphone_not_check'));
