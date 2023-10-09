9e31caa97b034327392f0940c355354fe1f6061f
wonjae <choi.wonjae@jinnhyun.com>
2023-10-10 06:40:49 +0900

    asdasd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,10 +26,10 @@ class HomeController extends Controller
     public function index()
     {
         $todayDate = Carbon::now()->timezone('Asia/Seoul')->format('Y-m-d');
-        $subdays = Carbon::now()->timezone('Asia/Seoul')->format('Y-m-d')->subDays(1);
+        $subdays = Carbon::now()->subDays(1)->timezone('Asia/Seoul')->format('Y-m-d');
 
         echo $subdays;
-        
+
         $user_cnt = DB::table('users')
                         ->where('created_at', 'like', $todayDate.'%')
                         ->count();
