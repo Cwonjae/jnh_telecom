747b14ec246740bacd48d777b92dbf5591f58f7b
wonjae <choi.wonjae@jinnhyun.com>
2023-10-10 13:23:03 +0900

    ㅂㅂㅂㅁㅁ

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/admin/{page}', [PageController::class, 'index'])->name('page');
 	Route::get('/admin/{page}/{num}', [PageController::class, 'print'])->name('page.print');
 	Route::post('/admin/{page}/checking/{num}', [PageController::class, 'comparison'])->name('page.comparisons');
-	Route::post('/admin/{page}/change/{num}/{status}', [PageController::class, 'status_change'])->name('page.statuschange');
+	Route::get('/admin/{page}/change/{num}/{status}', [PageController::class, 'status_change'])->name('page.statuschange');
 	// Route::get('/admin/comparisons-close', [PageController::class, 'close_alert'])->name('page.close_alert');
 	Route::post('/admin/logout', [LoginController::class, 'logout'])->name('logout');
 });
