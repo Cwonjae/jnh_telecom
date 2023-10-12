6573e0d9a16bf74aff2a8b619df40906987bd2aa
wonjae <choi.wonjae@jinnhyun.com>
2023-10-12 13:53:49 +0900

    qweqwe

--- resources/views/pages/user/tables_modify.blade.php
+++ resources/views/pages/user/tables_modify.blade.php
@@ -309,7 +309,7 @@
                                     @error('plan') <p class='text-danger text-xs'> {{ $message }} </p> @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
-                                    <h6 style="float:left;">Please choose the last four digits of your phone number (Up to 3 items can be entered)</h6><br><h4 id="choose_lang"></h4>
+                                    <h6>Please choose the last four digits of your phone number (Up to 3 items can be entered)</h6><h4 id="choose_lang"></h4>
                                     <input type="text" name="chooselastnumber" class="form-control" placeholder="Please choose the last four digits of your phone number." aria-label="Please choose the last four digits of your phone number." value="{{ $cell_phones[0]->cpb_chooselastnumber }}" onKeyPress="javascript:checkInputNum();" onKeyUp="javascript:checkInputValue(this);" maxlength="14" id="chooselastnumber">
                                     @error('chooselastnumber') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                     <p>If the phone number you have chosen is already taken, please note that it can be activated with different last four digits.</p>
