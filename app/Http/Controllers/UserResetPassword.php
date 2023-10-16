2baf36cdab640409d53491e144b88689e620b0a4
wonjae <choi.wonjae@jinnhyun.com>
2023-10-16 10:50:44 +0900

    user forgot add

--- app/Http/Controllers/UserResetPassword.php
+++ app/Http/Controllers/UserResetPassword.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
 use App\Models\User;
-use App\Notifications\ForgotPassword;
+use App\Notifications\UserForgotPassword;
 
 class UserResetPassword extends Controller
 {
@@ -28,7 +28,7 @@ class UserResetPassword extends Controller
         $user = User::where('email', $email)->first();
 
         if ($user) {
-            $this->notify(new ForgotPassword($user->id));
+            $this->notify(new UserForgotPassword($user->id));
             return back()->with('succes', 'An email was send to your email address');
         }
     }
