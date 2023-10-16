46ebd181614703027852c51adbae520852b6f37c
wonjae <choi.wonjae@jinnhyun.com>
2023-10-16 16:06:03 +0900

    qweqwe

--- resources/views/pages/print.blade.php
+++ resources/views/pages/print.blade.php
@@ -207,28 +207,7 @@
             }
         </style>
         <script>
-            (function() {
-                var beforePrint = function() {
-                    console.log('Functionality to run before printing.');
-                };
-
-                var afterPrint = function() {
-                    console.log('Functionality to run after printing');
-                };
-
-                if (window.matchMedia) {
-                    var mediaQueryList = window.matchMedia('print');
-                    mediaQueryList.addListener(function(mql) {
-                        if (mql.matches) {
-                            beforePrint();
-                        } else {
-                            afterPrint();
-                        }
-                    });
-                }
-                window.onbeforeprint = beforePrint;
-                window.onafterprint = afterPrint;
-            }());
+            window.onfocus=function(){ window.close();}
         </script>
     </head>
     <body onload="javascript:window.print();">
