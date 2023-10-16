0b839c73c05360617da97b1649654da48069ac9d
wonjae <choi.wonjae@jinnhyun.com>
2023-10-16 11:22:17 +0900

    ㅁㅇㅁㅇ

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
