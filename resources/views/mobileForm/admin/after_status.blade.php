eeddeb75c0346b805e1e5473f46bd98e93a5ca28
wonjae <choi.wonjae@jinnhyun.com>
2023-10-18 14:34:13 +0900

    qweqwe

--- resources/views/mobileForm/admin/after_status.blade.php
+++ resources/views/mobileForm/admin/after_status.blade.php
@@ -1,4 +1,4 @@
 <h4 class="font-weight-bolder">Please register your alien registration card on the website and apply for a postpaid payment</h4>
 <p class="mb-0">Shortcut to Home Page.
-    <a href="{{ route('userpage') }}">Link</a>
+    <a href="{{ route('userpage', ['page' => 'tables']) }}">Link</a>
 </p>
