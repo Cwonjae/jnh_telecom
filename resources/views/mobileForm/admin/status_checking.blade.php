9be47e69c00ece821e670047e54eb937635cd5f9
wonjae <choi.wonjae@jinnhyun.com>
2023-10-25 15:16:32 +0900

    나ㅣ엄닝

--- resources/views/mobileForm/admin/status_checking.blade.php
+++ resources/views/mobileForm/admin/status_checking.blade.php
@@ -9,7 +9,6 @@
     <p class="mb-0">신청자 : {{ $user_name }}</p>
     <p class="mb-0">승인 처리자 : {{ $admin_name_checks }}</p>
     <p class="mb-0">승인 처리자 이메일 : {{ $admin_email_checks }}</p>
-    <p class="mb-0">승인 처리자 이메일 : {{ $admin_email_checks }}</p>
     <p class="mb-0">승인 처리 일자 : {{ $now_date_time }}</p>
     </body>
 </html>
