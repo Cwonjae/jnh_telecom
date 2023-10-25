6ec5a0d817894eeb7cce18cebdfb943034cc00f4
wonjae <choi.wonjae@jinnhyun.com>
2023-10-25 16:05:49 +0900

    zxcasd

--- resources/views/pages/registration_card.blade.php
+++ resources/views/pages/registration_card.blade.php
@@ -8,13 +8,13 @@
             });
         });
     </script>
-    @include('layouts.navbars.auth.topnav', ['title' => 'Registration Card Comparison'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Alien registration card Comparison'])
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-12">
                 <div class="card mb-4">
                     <div class="card-header pb-0">
-                        <h6>Registration Card Comparison & Phone Number</h6>
+                        <h6>Alien registration card Comparison & Phone Number</h6>
                     </div>
                     <div class="card-body px-0 pt-0 pb-2">
                         <div class="table-responsive p-0" style="width:92%; margin:auto; margin-top:30px;">
@@ -26,7 +26,7 @@
                                         @error('phone_number') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
                                     </div>
                                     <div class="flex flex-col mb-3">
-                                        <h6>Saved Passport Images<span style="color:red">*</span></h6>
+                                        <h6>Saved Alien registration card Images<span style="color:red">*</span></h6>
                                         @if($idcard[0]->icu_filename)
                                         @php
                                             $check_extension = explode('.', $idcard[0]->icu_filename);
