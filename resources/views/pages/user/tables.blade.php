66f68a90f2a763188879b238e8bd674ccd2ce439
wonjae <choi.wonjae@jinnhyun.com>
2023-10-25 13:07:35 +0900

    tables page modify

--- resources/views/pages/user/tables.blade.php
+++ resources/views/pages/user/tables.blade.php
@@ -1,13 +1,50 @@
 @extends('layouts.user.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
-    @include('layouts.navbars.auth.user.topnav', ['title' => 'Olleh Prepaid Application Form'])
+    <script>
+        $(function () {
+            $('#lang_check').change(function() {
+                var value = $(this).val();
+                switch(value) {
+                    case 'english' :
+                        $('#name_lang').html("Full Name <span style='color:red'>*</span>");
+                        $('#nationality_lang').html("Nationality <span style='color:red'>*</span>");
+                        $('#passportnumber_lang').html("Passport Number <span style='color:red'>*</span>");
+                        $('#passport_lang').html("Passport <span style='color:red'>*</span>");
+                        $('#deteofbirth_lang').html("Dete of Birth <span style='color:red'>*</span>");
+                        $('#gender_lang').html("Gender <span style='color:red'>*</span>");
+                        $('#device_lang').html("Device <span style='color:red'>*</span>");
+                        break;
+                    case 'russian' :
+                        break;
+                    case 'uzbek' :
+                        break;
+                    case 'tagalog' :
+                        break;
+                    case 'vietnamese' :
+                        break;
+                    default :
+                        break;
+                }
+            });
+        });
+    </script>
+    @include('layouts.navbars.auth.user.topnav', ['title' => 'Prepaid Application Form'])
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-12">
                 <div class="card mb-4">
                     <div class="card-header pb-0">
-                        <h6>Olleh Prepaid Application Form</h6>
+                        <h6>Prepaid Application Form</h6>
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <select class="lang_check" id="lang_check">
+                            <option value="english" selected>English</option>
+                            <option value="russian">Russian</option>
+                            <option value="uzbek">Uzbek</option>
+                            <option value="tagalog">Tagalog</option>
+                            <option value="vietnamese">Vietnamese</option>
+                        </select>
                     </div>
                     <div class="pb-0">
                         <div style="float:right; width:120px; height:40px;">
@@ -89,7 +126,7 @@
                                     </tr>
                                     @empty
                                     <tr>
-                                        <td class="align-middle text-center" colspan="6">don't have a history of applying for Olleh Mobile Application Form</td>
+                                        <td class="align-middle text-center" colspan="6">don't have a history of applying for Prepaid Application Form</td>
                                     </tr>
                                     @endforelse
                                 </tbody>
